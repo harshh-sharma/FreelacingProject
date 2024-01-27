0d2b4b96367751f12aa2dfaa27710619a0adce7c
harshh-sharma <harsh877sharma@gmail.com>
2024-01-27 21:21:33 +0530

    add Intial files

---
+++ script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+var loader = document.getElementById('preloader');
+window.addEventListener("load",() => {
+  loader.style.display = 'none';
+})
